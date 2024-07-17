e88476430e65cd9afc522d65126afd702875749e
Arnaldo Liro <dinholiro@gmail.com>
2024-07-17 14:40:35 -0300

    list of demands

--- src/controllers/demandsController.ts
+++ src/controllers/demandsController.ts
@@ -1,9 +1,17 @@
 import database from '../database/database'
 import { FastifyRequest, FastifyReply } from 'fastify'
-import AuthController from './authController'
 
 class DemandController {
     async createNewDemand (request: FastifyRequest, reply: FastifyReply) {
+
+        const token = request.headers.authorization?.split(' ')[1]
+
+        const user = await database.user.findFirst({where: {token} })
+
+        if (!user) {
+            return reply.status(400).send({ error: 'Usuário não encontrado'})
+        }
+        
         const {title, description, status} = request.body as {
             title: string
             description: string
@@ -19,12 +27,43 @@ class DemandController {
                 data: {
                     title,
                     description,
+                    userId: user.id,
                     status: "Avaliando...",
                 },
             })
             reply.status(201).send(newDemand)
         } catch (error) {
             reply.status(500).send({ error: 'Ocorreu um erro na criação da demanda'})
+        }
+    }
+
+    async demandsList(request:FastifyRequest, reply: FastifyReply){
+        const token = request.headers.authorization?.split(' ')[1]
+
+        const user = await database.user.findFirst({where: {token} })
+
+        if (!user) {
+            return reply.status(400).send({ error: 'Usuário não encontrado'})
+        }
+
+        try {
+            const list = await database.demands.findMany({
+                where: {
+                    userId: user.id
+                },
+                select: {
+                    title: true,
+                    description: true,
+                    status: true,
+                    feedback: true,
+                    createdAt: true,
+                    updatedAt: true,
+                }
+            })
+
+            reply.status(201).send(list)
+        } catch(error) {
+            reply.status(400).send({error: 'Ocorreu um erro na listagem das demandas'})
         }
     }
 }
--- src/routes/demandsRoutes.ts
+++ src/routes/demandsRoutes.ts
@@ -5,6 +5,7 @@ const demandController = new DemandController()
 
 async function demandRoutes(fastify: FastifyInstance) {
     fastify.post('/create', demandController.createNewDemand)
+    fastify.get('/list', demandController.demandsList)
 }
 
 export default demandRoutes
